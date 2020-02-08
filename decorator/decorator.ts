16815f6c8ac33209b30cf885172a9a974e29bd43
iromashko <iromashko@me.com>
2020-02-08 09:13:54 +0300

    decorator refactor

--- decorator/decorator.ts
+++ decorator/decorator.ts
@@ -1,48 +1,35 @@
 namespace DecoratorPattern {
-
-    export interface Component {
-        operation(): void;
+  export interface Component {
+    operation(): void;
+  }
+  export class ConcreteComponent implements Component {
+    constructor(private s: string) {
+      this.s = s;
     }
-
-    export class ConcreteComponent implements Component {
-        private s: String;
-
-        constructor(s: String) {
-            this.s = s;
-        }
-
-        public operation(): void {
-            console.log("`operation` of ConcreteComponent", this.s, " is being called!");
-        }
+    public operation(): void {
+      console.log(`concrete component ${this.s}`);
     }
-
-    export class Decorator implements Component {
-        private component: Component;
-        private id: Number;
-
-        constructor(id: Number, component: Component) {
-            this.id = id;
-            this.component = component;
-        }
-
-        public get Id(): Number {
-            return this.id;
-        }
-
-        public operation(): void {
-            console.log("`operation` of Decorator", this.id, " is being called!");
-            this.component.operation();
-        }
+  }
+  export class Decorator implements Component {
+    constructor(private id: number, private component: Component) {
+      this.id = id;
+      this.component = component;
     }
-
-    export class ConcreteDecorator extends Decorator {
-        constructor(id: Number, component: Component) {
-            super(id, component);
-        }
-
-        public operation(): void {
-            super.operation();
-            console.log("`operation` of ConcreteDecorator", this.Id, " is being called!");
-        }
+    public get Id(): number {
+      return this.id;
     }
+    public operation(): void {
+      console.log(`decorator ${this.id}`);
+      this.component.operation();
+    }
+  }
+  export class ConcreteDecorator extends Decorator {
+    constructor(id: number, component: Component) {
+      super(id, component);
+    }
+    public operation(): void {
+      super.operation();
+      console.log(`concrete decorator ${this.Id}`);
+    }
+  }
 }
